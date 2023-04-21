79339471626dc6f23d12f057362819bb593c7ce6
Iakov Lilo <iakov.lilo@userware.onmicrosoft.com>
2023-04-21 15:58:22 +1000

    Add project files.

---
+++ OpenSilverWindowless/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OpenSilverWindowless
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            TextBlock1.Text = "Windowless - " + Application.Current.Host.Settings.Windowless;
+        }
+    }
+}
